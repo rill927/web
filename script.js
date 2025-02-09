f4f5ededa891df1b28fde422ba4875efac8adc39
rill927 <kontakjbril@gmail.com>
2025-02-09 11:27:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('whatsapp-btn').addEventListener('click', function() {
+    let phone = '6285840351569'; // Ganti dengan nomor tujuan
+    window.location.href = `https://wa.me/${phone}`;
+});
+
